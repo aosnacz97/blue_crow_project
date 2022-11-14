0b5638529c73a9898c9fca0fcc9c0e94446c5cde
anthony <111905957+aosnacz97@users.noreply.github.com>
2022-11-14 14:12:22 -0800

    Removed to-do list

--- note_pad.R
+++
@@ -1,6 +0,0 @@
-# to-do list
-# get jumps averaged for the day (or any repeat tests averaged, outliers removed)
-# get tab 3 done
-# fill in all the boxes/descriptions
-# answer questions
-# make it pretty
